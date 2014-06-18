b77b4b9ff817060e5513977a2210629e46439680
Roopesh Manjunatha <rmanjunatha@ebay.com>
2014-06-17 17:14:12 -0700

    Initial Commit

---
+++ DLImageLoader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLImageLoader
+//
+//  Created by Roopesh Manjunatha on 6/17/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLLoaderAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLLoaderAppDelegate class]));
+    }
+}
